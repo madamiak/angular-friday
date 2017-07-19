f4b976069427fc34344c86ff5f1541768089f145
Mateusz <amalgammatt@gmail.com>
2017-07-19 06:06:44 +0200

    Promise - broken test

---
+++ src/app/async-vs-fake-async/s01-flag-service.service.spec.ts
@@ -0,0 +1,19 @@
+import {S01FlagServiceService} from './s01-flag-service.service';
+
+describe('S01FlagServiceService', () => {
+
+  fit('some thing should happen when we do some thing', () => {
+
+    let flag = false;
+
+    const testPromise = new Promise((resolve) => {
+      setTimeout(() => {}, 100);
+    });
+
+    testPromise.then((result) => {
+      flag = true;
+    });
+
+    expect(flag).toBe(true);
+  });
+});
---
+++ src/app/async-vs-fake-async/s01-flag-service.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class S01FlagServiceService {
+
+  constructor() { }
+
+}
